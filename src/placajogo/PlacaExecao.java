33c176b326aa464ddf32ee9e5c3de33e6e2f6083
[Israelhrm1995] <isrealhrm1995@gmail.com>
2021-03-02 17:30:21 -0300

    Excecao do xadrez e posicao de xadrez

---
+++ src/placajogo/PlacaExecao.java
@@ -0,0 +1,10 @@
+package placajogo;
+
+public class PlacaExecao extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public PlacaExecao(String msg) {
+		super(msg);
+	}
+
+}
