2b97e686272d60f1585fd1f1fb947150a9b70604
[Israelhrm1995] <isrealhrm1995@gmail.com>
2021-03-02 19:53:30 -0300

    Pecas em movimento

--- src/xadrez/XadrezPosicao.java
+++ src/xadrez/XadrezPosicao.java
@@ -9,7 +9,7 @@ public class XadrezPosicao {
 	
 	public XadrezPosicao(char coluna, char linha) {
 		if (coluna < 'a' || coluna > 'h' || linha < 1 || linha > 8) {
-			throw new XadrezExecao("Erro ao criar uma posicao no xadres os valores permitidos sao de a1 a h8");
+			throw new XadrezExecao("Erro ao criar uma posicao no xadrez os valores permitidos sao de a1 a h8");
 		}
 		this.coluna = coluna;
 		this.linha = linha;
