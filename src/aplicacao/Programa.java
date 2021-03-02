e3523d6b8aa443b953824ee295df3b92a6994f73
[Israelhrm1995] <isrealhrm1995@gmail.com>
2021-03-02 15:37:14 -0300

    iniciando a implementacao do tabuleiro e da peca

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,14 +1,14 @@
 package aplicacao;
 
-import placajogo.Posicao;
+import placajogo.Tabuleiro;
 
 public class Programa {
 
 	public static void main(String[] args) {
 	
-		Posicao pos = new Posicao(3, 5);
-		System.out.println(pos);
 
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		
 	}
 
 }
