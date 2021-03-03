5537a2f7b4516342e7915d69aee65fd8ff44071e
[Israelhrm1995] <isrealhrm1995@gmail.com>
2021-03-03 11:34:31 -0300

    Bispo

--- src/xadrez/XadrezPartida.java
+++ src/xadrez/XadrezPartida.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import placajogo.Peca;
 import placajogo.Posicao;
 import placajogo.Tabuleiro;
+import xadrez.pecas.Bispo;
 import xadrez.pecas.Peao;
 import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
@@ -193,7 +194,9 @@ public class XadrezPartida {
 	
 	private void iniciarPartida() {
 		colocarNovaPeca('a', 1, new Torre(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('c', 1, new Bispo(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('h', 1, new Torre(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('a', 2, new Peao(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('b', 2, new Peao(tabuleiro, Cor.BRANCO));
@@ -204,9 +207,10 @@ public class XadrezPartida {
 		colocarNovaPeca('g', 2, new Peao(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('h', 2, new Peao(tabuleiro, Cor.BRANCO));
 		
-		
 		colocarNovaPeca('a', 8, new Torre(tabuleiro, Cor.PRETO));
+		colocarNovaPeca('c', 8, new Bispo(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
+		colocarNovaPeca('f', 8, new Bispo(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('h', 8, new Torre(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('a', 7, new Peao(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('b', 7, new Peao(tabuleiro, Cor.PRETO));
